2a135c6b5ef9e573b652579f0865ce90831d6120
akautomatadev <automatalab.contacto@gmail.com>
2023-09-21 10:58:40 -0600

    note taker with routes

---
+++ server.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const path = require('path');
+const fs = require('fs');
+const uniqid = require('uniqid')
+const { error } = require('console');
+const app = express();
+const port = 3001;
+
+app.use(express.static('public'));
+app.use(express.json());
+
+app.get('/notes' , (req,res)=>{
+    res.sendFile(path.join(__dirname,'/public/notes.html'));
+});
+
+
+app.get('/api/notes', (req,res) => {
+    fs.readFile('./db/db.json', 'utf8', (error,data) => {
+        res.send(data);
+    })
+})
+
+app.post('/api/notes', (req,res) => {
+    fs.readFile('./db/db.json', 'utf8', (error,data) => {
+    const newNote = req.body;
+    newNote.id = uniqid();
+    const notes = JSON.parse(data);
+    notes.push(newNote);
+    fs.writeFile('./db/db.json', JSON.stringify(notes), (error,data) =>{
+        res.json(newNote);
+    });
+    })
+})
+
+
+app.delete('/api/notes/:id', (req,res) => {
+    fs.readFile('./db/db.json', 'utf8', (error,data) => {
+    const notes = JSON.parse(data);
+    const updatedNotes = notes.filter(note => note.id !== req.params.id);
+    fs.writeFile('./db/db.json', JSON.stringify(updatedNotes), (error,data) =>{
+        res.json(notes);
+    });
+    })
+})
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname,'/public/index.html'));
+});
+
+app.listen(port, () =>  console.log(`Example app listening at http://localhost:${port}`) );
+
